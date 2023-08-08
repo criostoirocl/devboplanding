c4825134439cb114664f45fc1a162dd86967909d
Criostóir O'Codlatáin Lachtna <chris@wellola.com>
2023-08-08 10:31:40 +0100

    remove thumb

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,10 +86,7 @@ export default {
         site_name: 'DevBop',
         description:
           'Streamline your application deployment. Deploy applications into AWS and get a CI/CD pipeline in mere minutes. Reduce manual effort and accelerate your project timelines. Focus on creating, while we handle the complexities of AWS.',
-        img: 'thumb.png',
-        img_size: { width: '800px', height: '600px' },
         locale: 'en_US',
-        twitter: '@silvesterwali',
         twitter_card: 'DevBop - Deploy applications and CI/CD into AWS in minutes',
         theme_color: '#ffffff',
       },
